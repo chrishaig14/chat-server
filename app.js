a73edfd926e163705084b8f768bd60aaf3b8de69
chrishaig14 <chrishaig14@gmail.com>
2019-08-06 22:27:41 -0300

    Add basic login & disconnect

--- app.js
+++ app.js
@@ -31,11 +31,25 @@ function handleSignup(client, m) {
     db.collection("users").insertOne({user: m, contacts: []});
 }
 
-function handleLogin(client, m) {
+async function handleLogin(client, m) {
     clientUserMap[client.id] = m;
     userClientMap[m] = client.id;
     console.log("USER LOGGED IN: ", m);
     console.log("LOGGED USERS:", userClientMap);
+    let result = await db.collection("users").findOne({user: m});
+    console.log("LOGIN RESULT: ", result);
+    if (result !== null) {
+        client.emit("login:ok");
+    } else {
+        client.emit("login:error");
+    }
+}
+
+function handleDisconnect(client, m) {
+    console.log("USER ", clientUserMap[client.id], " DISCONNECTED");
+    let username = clientUserMap[client.id];
+    delete userClientMap[username];
+    delete clientUserMap[client.id];
 }
 
 function handleAddContact(client, m) {
@@ -43,7 +57,8 @@ function handleAddContact(client, m) {
 }
 
 function handleGetContacts(client, m) {
-    db.collection("users").find({});
+    let result = db.collection("users").find({user: clientUserMap[client.id]}, {contacts: 1});
+    client.emit("get:contacts", result);
 }
 
 io.on("connection", function (client) {
@@ -51,7 +66,9 @@ io.on("connection", function (client) {
     client.on("message", (m) => handleMessage(client, m));
     client.on("new:user", (m) => handleSignup(client, m));
     client.on("add:contact", (m) => handleAddContact(client, m));
+    client.on("get:contacts", (m) => handleGetContacts(client, m));
     client.on("login", (m) => handleLogin(client, m));
+    client.on("disconnect", (m) => handleDisconnect(client, m));
 });
 
 server.listen(3000);
